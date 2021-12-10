32f9c3c86e7bde918d0397efd0d5e5f96898eae9
Tfauves <tfauvelle@gmail.com>
2021-12-09 21:04:25 -0500

    auth controller.

---
+++ src/main/java/com/geekylikes/app/controllers/authController.java
@@ -0,0 +1,61 @@
+package com.geekylikes.app.controllers;
+
+import com.geekylikes.app.payload.request.LoginRequest;
+import com.geekylikes.app.payload.response.JwtResponse;
+import com.geekylikes.app.repositories.RoleRepository;
+import com.geekylikes.app.repositories.UserRepository;
+import com.geekylikes.app.security.jwt.JwtUtils;
+import com.geekylikes.app.security.services.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/auth")
+public class authController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse( jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                roles));
+    }
+
+}
+
+
